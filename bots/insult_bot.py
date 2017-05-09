ff3391ea44285699ea8768b04df4f619964b9243
JaanVr <jaan_veersoo@hotmail.com>
2017-05-09 23:08:29 +0300

    adder rick gif

--- bots/insult_bot.py
+++ bots/insult_bot.py
@@ -6,6 +6,8 @@ class InsultBot(bot):
             bot.send_photo(chat_id=update.message.chat_id, photo=open('arvamus.jpg', 'rb'))
         elif args[0] == "ultimate":
             bot.sendMessage(chat_id=update.message.chat_id, text="You... dirty... stuck-up... sadistic... shit-eating, cocksucking, buttfucking, penis-smelling, crotch-grabbing, ball-licking, semen-drinking, dog-raping, Nazi-loving, child-touching, cow-humping, perverted, spineless, heartless, mindless, dickless, testicle-choking, urine-gargling, jerk-offing, horse face, sheep-fondling, toilet-kissing, self-centered, feces-puking, dildo-shoving, snot-spitting, crap-gathering, big-nosed, monkey-slapping, bastard-screwing, bean-shitting, fart-knocking, sack-busting, splooge-tasting, bear-blowing, head-swallowing, bitch-snatching, handjobbing, donkey-caressing, mucus-spewing, anal-plugging, ho-grabbing, uncircumsized, sewer-sipping, whore mongering, piss-swimming, midget-munching, douchebag, ho-biting, carnivorous, mail-order prostituting asshole!")
+        elif args[0] == "rick":
+            bot.send_document(chat_id=update.message.chat_id, document=open('arvamus_rick.mp4', 'rb'))
         else:
             super(InsultBot, self).send_response(bot,update,args)
 
