862ad9a6e68fcd7b85af4deeac5cc6d27282c3d3
JaanVr <jaan_veersoo@hotmail.com>
2017-05-20 23:59:08 +0300

    added skiimoovi

--- bots/compliment_bot.py
+++ bots/compliment_bot.py
@@ -1,7 +1,13 @@
 from bot_backend import bot
 
+class ComplimentBot(bot):
+    def send_response(self, bot, update, args):
+        if args[0] == "skiimoovi":
+            bot.sendMessage(chat_id=update.message.chat_id, text="Norra on nii ilus vää")
+            bot.sendMessage(chat_id=update.message.chat_id, text="https://www.youtube.com/watch?v=Gn4ulQFtpI4")
+        else:
+            super(InsultBot, self).send_response(bot,update,args)
 filename = "compliment.txt"
 token='261401432:AAGLQFIehbRt6zH2TNYTJyvr2PUbnfYRcew'
 command = 'compliment'
-
-bot(token, filename, command)
+ComplimentBot(token, filename, command)
