aa2408e5ebd626d83cd5e3384bd9e9c39680a8fd
U-ADHARMAN\JVeersoo <JVeersoo@HITLN00C.ad.harman.com>
2017-05-23 14:35:05 +0300

    fixed a bug in super

--- bots/compliment_bot.py
+++ bots/compliment_bot.py
@@ -6,7 +6,7 @@ class ComplimentBot(bot):
             bot.sendMessage(chat_id=update.message.chat_id, text="Norra on nii ilus vää")
             bot.sendMessage(chat_id=update.message.chat_id, text="https://youtu.be/gD8Hs4xWVhQ")
         else:
-            super(InsultBot, self).send_response(bot,update,args)
+            super(ComplimentBot, self).send_response(bot,update,args)
 filename = "compliment.txt"
 token='261401432:AAGLQFIehbRt6zH2TNYTJyvr2PUbnfYRcew'
 command = 'compliment'
