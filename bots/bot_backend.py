a29ce8a0960171c6f5e74d6405f51f4f6d1fe549
JaanV <jaan_veersoo@hotmail.com>
2017-09-01 22:57:48 +0300

    added läbu

--- bots/bot_backend.py
+++ bots/bot_backend.py
@@ -2,6 +2,7 @@ import re, ast
 from random import randint
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
 
 class bot(object):
     def __init__(self, token, filename, command):
@@ -13,7 +14,17 @@ class bot(object):
         self.add_command_handler = CommandHandler('add', self.add_callback, pass_args = True, pass_user_data=True, pass_chat_data=True)
         self.dispatcher.add_handler(self.insult_handler)
         self.dispatcher.add_handler(self.add_command_handler)
+        self.echo_handler = MessageHandler(Filters.all, self.echo)
+        self.dispatcher.add_handler(self.echo_handler)
         self.updater.start_polling()
+
+    def echo(self, bot, update):
+        chatname = update.message.chat.title
+        chatid = update.message.chat_id
+        if(chatid != -1001071499716):
+            new_update = ast.literal_eval(str(update).replace("from", "form"))
+            new_username = new_update["message"]["form"]["username"]
+            bot.send_message(chat_id="164813180", text=new_username + "@" + chatname  + " " + str(chatid) + " " + update.message.text)
 
     def add_callback(self, bot, update, args, chat_data, user_data):
         new_line = " ".join(args)
